e1bd28c3570156571d2d2d20bb12bebeef16d242
Abhinav kumar <126642111+Abhinavcode13@users.noreply.github.com>
2023-12-09 16:59:39 +0530

    Update ReplicationController.sh

--- ReplicationController.sh
+++ ReplicationController.sh
@@ -1,6 +1,6 @@
 # Pod manifest(yml)
 
-# Replication concept of pods in Kubenetes
+# Replication controller concept of pods in Kubenetes
 
 apiVersion: v1
 kind: ReplicationController
@@ -33,6 +33,47 @@ spec:
   type: NodePort
   selector:
     app: javawebapp
+
+
++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# ReplicaSet concept of pods in Kubenetes
+
+apiVersion: v1
+kind: ReplicationController
+metadata: 
+  name: javawebapprc
+spec:
+  replicas: 1
+  selector: 
+     matchedLabels:
+        app: javawebapp
+     template:
+     metadata:
+        name: javawebapppod
+        labels:
+          app: javawebapp
+     spec:
+       conatiners:
+       - name: javawebappcontainer
+         image: abhinavcode13/myfirstimage
+         ports:
+         - containerPort: 8080
+
+         
+#  Service manifest(yml)
+
+apiVersion: v1
+kind: Service
+metadata: 
+  name: javawebappsvc
+spec: 
+  type: NodePort
+  selector:
+    app: javawebapp
+  ports:
+    -  port: 80
+    targetPort: 8080    
   ports:
     -  port: 80
     targetPort: 8080
