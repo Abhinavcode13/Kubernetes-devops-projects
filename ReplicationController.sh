17a38dc4b78e56886c3e9b5f989d80a96c1b3d21
Abhinav kumar <126642111+Abhinavcode13@users.noreply.github.com>
2023-12-09 17:15:58 +0530

    Update ReplicationController.sh

--- ReplicationController.sh
+++ ReplicationController.sh
@@ -77,3 +77,18 @@ spec:
   ports:
     -  port: 80
     targetPort: 8080
+
+
++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+#DeamonSet concept for pods in kubernetes
+
+-> A DaemonSet ensures that all (or some) Nodes run a copy of a Pod. 
+-> As nodes are added to the cluster, Pods are added to them. As nodes are removed from the cluster, those Pods are garbage collected. 
+-> Deleting a DaemonSet will clean up the Pods it created.
+
+Some typical uses of a DaemonSet are:
+
+1. running a cluster storage daemon on every node
+2. running a logs collection daemon on every node
+3. running a node monitoring daemon on every node
